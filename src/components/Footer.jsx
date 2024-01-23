bbbce46a7a98ee7a0b1bcf23c135864607256f8f
only1tarunno <tarunno281@gmail.com>
2024-01-23 20:03:29 +0600

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-6 md:py-8">
       <div className="container mx-auto">
         <p className="text-center text-sm lg:text-base">
-          Copyright ©2023 | All rights reserved by Learn with Sumit
+          Copyright ©2023 | All rights reserved by Tarunno
         </p>
       </div>
     </footer>
